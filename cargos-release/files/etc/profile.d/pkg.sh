52ced77b3106be7ca60acb4350bf681baf559dd7
Antoine Jacoutot <antoine@redcoolbeans.com>
2015-03-18 09:14:36 +0100

    Do not inherit old PATH. Do not hardcode the machine hardware name. export PKG_REPOS instead of in-place editing of repositories.conf for pkgin(1).

--- cargos-release/files/etc/profile.d/pkg.sh
+++ cargos-release/files/etc/profile.d/pkg.sh
@@ -2,15 +2,19 @@
 # @EGDIR@/etc/profile.d/pkg.sh
 
 # PATH
+PATH=/usr/bin:/bin:/usr/sbin:/sbin			# RO base
 PATH=${PATH}:@LOCALBASE@/sbin:@LOCALBASE@/bin			# pkg PREFIX
 #PATH=${PATH}:@LOCALBASE@/gnu/bin				# GNU overrides
 PATH=${PATH}:@LOCALBASE@/gcc47/bin				# GCC
 
 # MANPATH
-MANPATH=/usr/share/man						# buildroot
+MANPATH=/usr/share/man					# buildroot
 MANPATH=${MANPATH}:@LOCALBASE@/man				# pkg PREFIX
 MANPATH=${MANPATH}:@LOCALBASE@/gcc47/man			# GCC
 MANPATH=${MANPATH}:@LOCALBASE@/lib/perl5/vendor_perl/man	# perl
-MANPATH=${MANPATH}:@LOCALBASE@/lib/perl5/man			# perl
+MANPATH=${MANPATH}:@LOCALBASE@/lib/perl5/man		# perl
 
-export MANPATH PATH
+# PKG_REPOS (see pkgin(1))
+PKG_REPOS=http://cargos.io/pub/pkgsrc/packages/CargOS/@MACHINE@/2014Q4/All
+
+export MANPATH PATH PKG_REPOS
