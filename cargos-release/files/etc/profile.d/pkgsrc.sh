4186474d472cfbd6134d52546fb6184628e7c511
Antoine Jacoutot <antoine@redcoolbeans.com>
2015-05-05 19:02:28 +0200

    All branched.

--- cargos-release/files/etc/profile.d/pkgsrc.sh
+++
@@ -1,5 +0,0 @@
-# @PKGNAME@
-# @EGDIR@/etc/profile.d/pkgsrc.sh
-
-CVS_RSH=ssh
-export CVS_RSH
--- cargos-release/files/etc/profile.d/sendmail.sh
+++
@@ -1,7 +0,0 @@
-# @PKGNAME@
-# @EGDIR@/etc/profile.d/sendmail.sh
-
-# SMTP relay host for busybox(1) sendmail (default: 127.0.0.1)
-#SMTPHOST=
-
-export SMTPHOST
