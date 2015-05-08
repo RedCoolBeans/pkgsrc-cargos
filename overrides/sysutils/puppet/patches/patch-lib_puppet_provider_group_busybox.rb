f54151d368262c0c553e5e882000cc7b8e4532ea
Jasper Lievisse Adriaanse <jasper@redcoolbeans.com>
2015-05-08 17:42:29 +0200

    Implement #instances for user/group and #create/#delete for users.
    No password management or anything beyond creating a user yet.

--- overrides/sysutils/puppet/patches/patch-lib_puppet_provider_group_busybox.rb
+++ overrides/sysutils/puppet/patches/patch-lib_puppet_provider_group_busybox.rb
@@ -2,24 +2,32 @@ $NetBSD$
 
 --- lib/puppet/provider/group/busybox.rb.orig
 +++ lib/puppet/provider/group/busybox.rb
-@@ -0,0 +1,20 @@
-+require 'puppet/provider/nameservice/objectadd'
-+require 'date'
-+require 'time'
+@@ -0,0 +1,28 @@
++require 'puppet/provider/nameservice'
 +require 'puppet/error'
 +
-+Puppet::Type.type(:group).provide :busybox, :parent => Puppet::Provider::NameService::ObjectAdd do
++Puppet::Type.type(:group).provide :busybox, :parent => Puppet::Provider::NameService do
 +  desc "Local group management for Busybox"
 +
 +  commands :busybox => "busybox"
 +
-+  def addmcd
-+    debug "Called addcmd"
-+    busybox(:addgroup)
++  def exists?
++    groups = []
++    Etc.send("setgrent")
++    begin
++      while ent = Etc.send("getgrent")
++        return true if ent.name == @resource[:name]
++      end
++    rescue
++      false
++    end
 +  end
 +
-+  def delcmd
-+    debug "Called delcmd"
-+    busybox(:delgroup)
++  # lib/puppet/provider/nameservice/objectadd.rb#posixmethod
++  def posixmethod(name)
++    name = name.intern if name.is_a? String
++    method = self.class.option(name, :method) || name
++
++    method
 +  end
 +end
--- overrides/sysutils/puppet/patches/patch-lib_puppet_provider_user_busybox.rb
+++ overrides/sysutils/puppet/patches/patch-lib_puppet_provider_user_busybox.rb
@@ -2,24 +2,59 @@ $NetBSD$
 
 --- lib/puppet/provider/user/busybox.rb.orig
 +++ lib/puppet/provider/user/busybox.rb
-@@ -0,0 +1,20 @@
-+require 'puppet/provider/nameservice/objectadd'
-+require 'date'
-+require 'time'
+@@ -0,0 +1,55 @@
++require 'puppet/provider/nameservice'
 +require 'puppet/error'
 +
-+Puppet::Type.type(:user).provide :busybox, :parent => Puppet::Provider::NameService::ObjectAdd do
++Puppet::Type.type(:user).provide :busybox, :parent => Puppet::Provider::NameService do
 +  desc "Local user management for Busybox"
 +
 +  commands :busybox => "busybox"
 +
-+  def addmcd
-+    debug "Called addcmd"
-+    busybox(:adduser)
++  def create
++    debug "creating #{@resource[:name]}"
++    busybox(:adduser, "-D", @resource[:name])
 +  end
 +
-+  def delcmd
-+    debug "Called delcmd"
-+    busybox(:deluser)
++  def delete
++    debug "deleting #{@resource[:name]}"
++    busybox(:deluser, @resource[:name])
++  end
++
++  def exists?
++    begin
++      Etc.getpwnam(@resource[:name])
++    rescue
++      false
++    end
++  end
++
++  def home
++    self.getpwnam(:dir)
++  end
++
++  def uid
++    self.getpwnam(:uid)
++  end
++
++  def gid
++    self.getpwnam(:gid)
++  end
++
++  # XXXTODO
++  def comment
++  end
++
++  # XXXTODO
++  def groups
++  end
++
++  def getpwnam(field)
++    begin
++      entry = Etc.getpwnam(@resource[:name])
++      entry[field] if entry.respond_to?(field)
++    rescue
++      return
++    end
 +  end
 +end
