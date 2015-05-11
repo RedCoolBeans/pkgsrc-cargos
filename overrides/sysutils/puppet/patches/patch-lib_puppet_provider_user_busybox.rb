c0277f2cb1c1392d51af2900b5e1888727e58094
Jasper Lievisse Adriaanse <jasper@redcoolbeans.com>
2015-05-11 16:35:24 +0200

    Fairly complete user management for Puppet
    shell, homedir, uid, password can be set.
    password expiration is not handled yet.

--- overrides/sysutils/puppet/patches/patch-lib_puppet_provider_user_busybox.rb
+++ overrides/sysutils/puppet/patches/patch-lib_puppet_provider_user_busybox.rb
@@ -2,23 +2,52 @@ $NetBSD$
 
 --- lib/puppet/provider/user/busybox.rb.orig
 +++ lib/puppet/provider/user/busybox.rb
-@@ -0,0 +1,55 @@
+@@ -0,0 +1,92 @@
 +require 'puppet/provider/nameservice'
 +require 'puppet/error'
++require 'open3'
 +
 +Puppet::Type.type(:user).provide :busybox, :parent => Puppet::Provider::NameService do
 +  desc "Local user management for Busybox"
 +
 +  commands :busybox => "busybox"
++  # :manages_passwords requires Busybox to be compiled with FEATURE_SHADOWPASSWDS
++  has_features :manages_shell, :system_users, :manages_passwords
 +
 +  def create
 +    debug "creating #{@resource[:name]}"
-+    busybox(:adduser, "-D", @resource[:name])
++    opts = ["-D"]
++    opts += ["-h", @resource[:home]] if @resource[:home]
++    opts += ["-S"] if @resource[:system]
++    opts += ["-u", @resource[:uid]] if @resource[:uid]
++    opts += ["-s", @resource[:shell]] if @resource[:shell]
++    opts += ["-g", @resource[:comment]] if @resource[:comment]
++
++    busybox(:adduser, opts.flatten.compact, @resource[:name])
++
++    # Now set the password if needed
++    self.password = @resource[:password] if @resource[:password]
 +  end
 +
 +  def delete
 +    debug "deleting #{@resource[:name]}"
 +    busybox(:deluser, @resource[:name])
++  end
++
++  def password=(pwhash)
++    # XXX: Feels like a hack but Shadow::Passwd.sgetspent() is unusable for it crashes:
++    # *** Error in `irb': free(): invalid pointer: 0x00007fc9103b2be0 ***
++    stdin, stdout, stderr = Open3.popen3("echo \"#{resource[:name]}:#{resource[:password]}\" | chpasswd -e")
++    stdin.close
++  end
++
++  def password
++    if Puppet.features.libshadow?
++      if ent = Shadow::Passwd.getspnam(@resource[:name])
++        return unmunge(:password, ent.send(:sp_pwdp))
++      end
++    end
++    :absent
 +  end
 +
 +  def exists?
@@ -56,5 +85,13 @@ $NetBSD$
 +    rescue
 +      return
 +    end
++  end
++
++  # lib/puppet/provider/nameservice/objectadd.rb#posixmethod
++  def posixmethod(name)
++    name = name.intern if name.is_a? String
++    method = self.class.option(name, :method) || name
++
++    method
 +  end
 +end
