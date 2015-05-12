4566e08433f9b44936840b59aff60ddda3f967a5
Jasper Lievisse Adriaanse <jasper@redcoolbeans.com>
2015-05-12 20:48:19 +0200

    Rework how we check for enabled services, just because the file exists doesn't make the service enabled..

--- overrides/sysutils/puppet/patches/patch-lib_puppet_provider_service_rcng.rb
+++ overrides/sysutils/puppet/patches/patch-lib_puppet_provider_service_rcng.rb
@@ -5,7 +5,7 @@ New provider for RCng which uses the correct content to enable a service and pre
 
 --- lib/puppet/provider/service/rcng.rb.orig
 +++ lib/puppet/provider/service/rcng.rb
-@@ -0,0 +1,22 @@
+@@ -0,0 +1,38 @@
 +Puppet::Type.type(:service).provide :rcng, :parent => :bsd do
 +  desc <<-EOT
 +    RCng service management with rc.d
@@ -17,13 +17,29 @@ New provider for RCng which uses the correct content to enable a service and pre
 +    "/etc/rc.d"
 +  end
 +
++  # if the service file exists in rc.conf.d AND matches an expected pattern
++  # then it's already enabled
++  def enabled?
++    rcfile = File.join(rcconf_dir, @resource[:name])
++    if Puppet::FileSystem.exist?(rcfile)
++      File.open(rcfile).readlines.each do |line|
++        # Now look for something that looks like "service=${service:=YES}" or "service=YES"
++        if matchdata = line.match(/^#{@resource[:name]}=(?:YES|\${#{@resource[:name]}:=YES})/)
++          return :true
++        end
++      end
++    end
++
++    :false
++  end
++
 +  # enable service by creating a service file under rc.conf.d with the
 +  # proper contents
 +  def enable
 +    debug "Enabling"
 +    Dir.mkdir(rcconf_dir) if not Puppet::FileSystem.exist?(rcconf_dir)
 +    rcfile = File.join(rcconf_dir, @resource[:name])
-+    File.open(rcfile, File::WRONLY | File::APPEND | File::CREAT, 0644) { |f|
++    File.open(rcfile, File::WRONLY | File::CREAT, 0644) { |f|
 +      f << "%s=${%s:=YES}\n" % [@resource[:name], @resource[:name]]
 +    }
 +  end
