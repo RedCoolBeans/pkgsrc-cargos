eaa35a47e8996093f7e99c9b7dcad43f06b6ee9e
Jasper Lievisse Adriaanse <jasper@redcoolbeans.com>
2015-05-13 16:06:16 +0200

    since the service provider now knows about file contents, refuse to overwrite files
    to be fixed shortly

--- overrides/sysutils/puppet/patches/patch-lib_puppet_provider_service_rcng.rb
+++ overrides/sysutils/puppet/patches/patch-lib_puppet_provider_service_rcng.rb
@@ -5,7 +5,7 @@ New provider for RCng which uses the correct content to enable a service and pre
 
 --- lib/puppet/provider/service/rcng.rb.orig
 +++ lib/puppet/provider/service/rcng.rb
-@@ -0,0 +1,38 @@
+@@ -0,0 +1,42 @@
 +Puppet::Type.type(:service).provide :rcng, :parent => :bsd do
 +  desc <<-EOT
 +    RCng service management with rc.d
@@ -34,13 +34,17 @@ New provider for RCng which uses the correct content to enable a service and pre
 +  end
 +
 +  # enable service by creating a service file under rc.conf.d with the
-+  # proper contents
++  # proper contents, or by modifying it's contents to to enable the service.
 +  def enable
 +    debug "Enabling"
 +    Dir.mkdir(rcconf_dir) if not Puppet::FileSystem.exist?(rcconf_dir)
 +    rcfile = File.join(rcconf_dir, @resource[:name])
-+    File.open(rcfile, File::WRONLY | File::CREAT, 0644) { |f|
-+      f << "%s=${%s:=YES}\n" % [@resource[:name], @resource[:name]]
-+    }
++    if Puppet::FileSystem.exist?(rcfile)
++      fail("#{rcfile} exists, cowardly refusing to overwrite until #enable is adjusted")
++    else
++      File.open(rcfile, File::WRONLY | File::CREAT, 0644) { |f|
++        f << "%s=${%s:=YES}\n" % [@resource[:name], @resource[:name]]
++      }
++    end
 +  end
 +end
