6b63e2669561d6b481aa408d2b9c5b60ee2ada04
Jasper Lievisse Adriaanse <jasper@redcoolbeans.com>
2015-06-02 09:33:08 +0200

    Use Puppet::Util.replace_file for added safety

--- overrides/sysutils/puppet/patches/patch-lib_puppet_provider_service_rcng.rb
+++ overrides/sysutils/puppet/patches/patch-lib_puppet_provider_service_rcng.rb
@@ -5,7 +5,7 @@ New provider for RCng which uses the correct content to enable a service and pre
 
 --- lib/puppet/provider/service/rcng.rb.orig
 +++ lib/puppet/provider/service/rcng.rb
-@@ -0,0 +1,49 @@
+@@ -0,0 +1,51 @@
 +Puppet::Type.type(:service).provide :rcng, :parent => :bsd do
 +  desc <<-EOT
 +    RCng service management with rc.d
@@ -24,7 +24,7 @@ New provider for RCng which uses the correct content to enable a service and pre
 +    if Puppet::FileSystem.exist?(rcfile)
 +      File.open(rcfile).readlines.each do |line|
 +        # Now look for something that looks like "service=${service:=YES}" or "service=YES"
-+        if matchdata = line.match(/^#{@resource[:name]}=(?:YES|\${#{@resource[:name]}:=YES})/)
++        if line.match(/^\s*#{@resource[:name]}=(?:YES|\${#{@resource[:name]}:=YES})/)
 +          return :true
 +        end
 +      end
@@ -42,16 +42,18 @@ New provider for RCng which uses the correct content to enable a service and pre
 +    if Puppet::FileSystem.exist?(rcfile)
 +      newcontents = []
 +      File.open(rcfile).readlines.each do |line|
-+        if matchdata = line.match(/^#{@resource[:name]}=(NO|\$\{#{@resource[:name]}:NO\})/)
++        if line.match(/^\s*#{@resource[:name]}=(NO|\$\{#{@resource[:name]}:NO\})/)
 +          line = "#{@resource[:name]}=${#{@resource[:name]}:=YES}"
 +        end
 +        newcontents.push(line)
 +      end
-+      File.open(rcfile, 'w') { |file| file.puts newcontents }
++      Puppet::Util.replace_file(rcfile, 0644) do |f|
++        f.puts newcontents
++      end
 +    else
-+      File.open(rcfile, File::WRONLY | File::CREAT, 0644) { |f|
-+        f << "%s=${%s:=YES}\n" % [@resource[:name], @resource[:name]]
-+      }
++      Puppet::Util.replace_file(rcfile, 0644) do |f|
++        f.puts "%s=${%s:=YES}\n" % [@resource[:name], @resource[:name]]
++      end
 +    end
 +  end
 +end
