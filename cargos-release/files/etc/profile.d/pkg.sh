ab84ff89120cef3e6c7c76e0db3567a15b932441
Antoine Jacoutot <antoine@redcoolbeans.com>
2015-06-08 15:37:51 +0200

    Bump to gcc 4.9.

--- cargos-release/files/etc/profile.d/pkg.sh
+++ cargos-release/files/etc/profile.d/pkg.sh
@@ -5,12 +5,12 @@
 PATH=/usr/bin:/bin:/usr/sbin:/sbin			# RO base
 PATH=${PATH}:@LOCALBASE@/sbin:@LOCALBASE@/bin			# pkg PREFIX
 #PATH=${PATH}:@LOCALBASE@/gnu/bin				# GNU overrides
-PATH=${PATH}:@LOCALBASE@/gcc47/bin				# GCC
+PATH=${PATH}:@LOCALBASE@/gcc49/bin				# GCC
 
 # MANPATH
 MANPATH=/usr/share/man					# buildroot
 MANPATH=${MANPATH}:@LOCALBASE@/man				# pkg PREFIX
-MANPATH=${MANPATH}:@LOCALBASE@/gcc47/man			# GCC
+MANPATH=${MANPATH}:@LOCALBASE@/gcc49/man			# GCC
 MANPATH=${MANPATH}:@LOCALBASE@/lib/perl5/vendor_perl/man	# perl
 MANPATH=${MANPATH}:@LOCALBASE@/lib/perl5/man		# perl
 
