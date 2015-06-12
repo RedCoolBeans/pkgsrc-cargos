811cdcf0e19663c8113ade31f99916a9689048a5
Antoine Jacoutot <antoine@redcoolbeans.com>
2015-06-12 11:45:24 +0200

    SECURITY update to libressl-2.2.0.

--- overrides/security/openssl/buildlink3.mk
+++ overrides/security/openssl/buildlink3.mk
@@ -7,8 +7,8 @@ OPENSSL_BUILDLINK3_MK:=
 
 .  include "../../mk/bsd.fast.prefs.mk"
 
-BUILDLINK_API_DEPENDS.openssl=	libressl>=2.1.6
-BUILDLINK_ABI_DEPENDS.openssl=	libressl>=2.1.6
+BUILDLINK_API_DEPENDS.openssl=	libressl>=2.2.0
+BUILDLINK_ABI_DEPENDS.openssl=	libressl>=2.2.0
 BUILDLINK_PKGSRCDIR.openssl?=	../../security/openssl
 
 SSLBASE=	${BUILDLINK_PREFIX.openssl}
