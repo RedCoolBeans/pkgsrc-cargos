2a28009c16bf727f2423400228351dcdfbeb7f58
Jasper Lievisse Adriaanse <jasper@redcoolbeans.com>
2015-11-17 14:54:58 +0100

    Update to curl-7.44.0 (sync with pkgsrc_2015Q3)

--- www/curl/patches/patch-lib_hostcheck.c
+++ www/curl/patches/patch-lib_hostcheck.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_hostcheck.c,v 1.1 2014/03/31 08:45:21 wiz Exp $
+$NetBSD: patch-lib_hostcheck.c,v 1.4 2015/08/08 02:44:16 spz Exp $
 
 Add missing header for DragonflyBSD.
 PR 48691 by David Shao.
 
---- lib/hostcheck.c.orig
+--- lib/hostcheck.c.orig	2015-06-03 07:38:20.000000000 +0000
 +++ lib/hostcheck.c
 @@ -34,6 +34,13 @@
  #include "inet_pton.h"
